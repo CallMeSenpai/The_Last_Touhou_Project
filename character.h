41a563540a237cefe8b7927ecc14414d6a392d5c
CallMeSenpai <ericliao10797@hotmail.com>
2015-01-09 00:12:32 -0500

    :P

--- character.h
+++ character.h
@@ -1,11 +1,7 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-#include "include/SDL.h"
-
 void set_default_values_c(character*);
-void key_up(SDL_Event);
-void key_down(SDL_Event);
 void handle_input(character*);
 
 #endif
